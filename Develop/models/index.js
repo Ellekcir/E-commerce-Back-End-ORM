2595ae342fbd43694c593db88536531e5e65ba84
Rickelle <rickelle.1993@live.com.au>
2022-12-04 03:33:33 +1030

    index.js done

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: 'ProductTag',
+  foreignKey: 'product_id',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: 'ProductTag',
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
@@ -18,3 +29,20 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+
+// Products are conected to the category by the category_id
+//  {
+//   product_name: 'Plain T-Shirt',
+//   price: 14.99,
+//   stock: 14,
+//   category_id: 1,
+// } 
+// is connected to catergory 1 in the seeds:
+// {
+//   category_name: 'Shirts',
+// },
+
+// Category has many products which is also known to be the 'parent' again uses the category_id
+
+// the product tag belongs to each other connecting data in ProductTag combining the product_id and tag_id into one
